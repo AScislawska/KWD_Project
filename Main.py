8e6513e6c64356f44e6571041697b391f3a23a8b
WSudul <wojciech.sudul@gmail.com>
2018-01-27 16:57:06 +0100

    adding load_boston set

--- Main.py
+++ Main.py
@@ -1,3 +1,33 @@
-#1. Uczenie liniowej hipotezy dla problemu przewidywania cen nieruchomości (Housing Data)
+# 1. Uczenie liniowej hipotezy dla problemu przewidywania cen nieruchomości (Housing Data)
+
+from sklearn.datasets import load_boston
+
+boston = load_boston()
+
+features=boston.feature_names
+print(features)
+print(len(features))
+print('-----------------')
+print(boston.data)
+
+print(len(boston.data))
+invalid_rows = dict()
+print('-----------------')
+for idx, val in enumerate(boston.data):
+    if len(val) != len(features):
+        invalid_rows[idx] = (len(val))
+
+if invalid_rows:
+    print('invalid sets:')
+    print(invalid_rows)
+    quit(0)
+else:
+    print("all sets have data")
+
+print('-----------------')
+print(boston.target)
+print(len(boston.target))
+
+
 
 print("let's do this")
